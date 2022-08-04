7bd3f5a91fb95b9909cc80d07987c48c74d2ea28
Jhon <jhon@mail.com>
2022-08-04 10:19:18 +0300

    Повторение урока 39

--- video-039/script.js
+++ video-039/script.js
@@ -1,3 +1,58 @@
 "use strict";
 
+//* setInterval - setTimeout
 
+//* Вызов анонимной функции через 2сек.
+// const timerId = setTimeout(function(text) {
+//     console.log(text);
+// }, 2000, 'Hello');
+
+//* Передача готовой функции - без вызова «()» функции 
+// const getLogger = setTimeout(logger, 2000);
+
+//* очищение интервала
+// clearInterval(timerId);
+
+const btn = document.querySelector('.btn');
+let getLogger,
+    i = 0;
+
+// function logger() {
+//     if (i === 3) {
+//         clearInterval(getLogger);
+//     }
+//     console.log('text');
+//     i++;
+// }
+
+// btn.addEventListener('click', () => {
+//     // const timerId = setTimeout(logger, 2000);
+//     getLogger = setInterval(logger, 500);
+// });
+
+
+//* Рекурсивный пример setInterval
+//* Когда выполняться все действия произойдет повтор строго через 5сек.
+// let id = setTimeout(function log() {
+//     console.log('Recursion');
+//     id = setTimeout(log, 500);
+// }, 500);
+
+function myAnimation() {
+    const elem = document.querySelector('.box');
+    let pos = 0;
+
+    const id = setInterval(frame, 10);
+
+    function frame() {
+        if (pos == 300) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + 'px';
+            elem.style.left = pos + 'px';
+        }
+    }
+}
+
+btn.addEventListener('click', myAnimation);
