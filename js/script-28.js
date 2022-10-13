153e4094c5a56260d0feb4ba049f9620af57128f
Jhon <jhon@mail.com>
2022-10-13 15:45:52 +0300

    Pre-record lesson script

--- js/script-28.js
+++ js/script-28.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const box = document.getElementById('box');
+console.log(box);
+
+const btns = document.getElementsByTagName('button');
+console.log(btns);
+btns[0].style.background = 'tomato';
+
+const circles = document.getElementsByClassName('circle');
+console.log(circles);
+
+
+
+//* Современные методы получения элементов со страницы
+
+const hearts = document.querySelectorAll('.heart');
+console.log(hearts);
+hearts.forEach(heart => {
+    console.log(heart);
+});
+
+// Получение первого элемента со страницы
+const oneHeart = document.querySelector('.heart');
+console.log(oneHeart);
+
+const btn = document.querySelector('button');
+console.log(btn);
