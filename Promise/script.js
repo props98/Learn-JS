96fa30edcb159d7f9fd455a1f2a314b53b519e40
Jhon <jhon@mail.com>
2022-10-14 09:43:12 +0300

    add scripts

--- Promise/script.js
+++ Promise/script.js
@@ -1,2 +1,61 @@
 'use strict';
 
+//* Promise
+//* .then(() => {}) // .catch(() => {}) // .finally(() => {})
+
+console.log('Запрос данных...');
+
+// Обещание конструктор
+const req = new Promise(function(resolve, reject) { 
+    setTimeout(() => {
+
+        console.log('Подготовка данных...');
+    
+        const product = {
+            name: 'Tv',
+            price: 2000
+        };
+        
+        resolve(product);
+
+    }, 2000);
+}); 
+
+req.then((product) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            product.status = 'order';
+            // reject();
+            resolve(product);
+        }, 2000);
+    });
+}).then(data => {
+    data.modify = true;
+    return data;
+}).then((data) => {
+    console.log(data);
+}).catch(() => {
+    console.error('Произошла ошибка!');
+}).finally(() => {
+    console.log('Finally');
+});
+
+
+////////////////////////*
+
+const test = time => {
+    return new Promise(resolve => {
+        setTimeout(() => resolve(), time);
+    });
+};
+
+// test(1000).then(() => console.log('1000'));
+// test(1000).then(() => console.log('2000'));
+
+// Promise.all([test(1000), test(2000)]).then(() => {
+//     console.log('All');
+// });
+
+Promise.race([test(1000), test(2000)]).then(() => {
+    console.log('All');
+});
