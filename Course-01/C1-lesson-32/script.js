d9b48cbb79b2770be28bbd7ab8b091007616d66d
Jhon <jhon@mail.com>
2022-10-18 09:37:07 +0300

    passed lesson

--- Course-01/C1-lesson-32/script.js
+++ Course-01/C1-lesson-32/script.js
@@ -1,46 +1,37 @@
 "use strict";
 
-
 //? Отталкиваемся от родителя
+// console.log(document.head);
 // console.log(document.body);
 // console.log(document.documentElement);
-// console.log(document.body.childNodes); //! Nodelist
-// console.log(document.body.firstChild);
-// console.log(document.body.lastChild);
+// console.log(document.body.childNodes); //!NODElist
+// console.log(document.body.firstChild); //! Перенос строки
+// console.log(document.body.lastChild); //! Последний элемент скрипт
 
-//! Получение элемента 
-// console.log(document.body.firstElementChild);
-// console.log(document.body.lastElementChild);
+// console.log(document.body.firstElementChild); // первый элемент body
+// console.log(document.body.lastElementChild); // последний элемент body
 
-
+//! Получение элемента
 //? Получение родителя от ребенка
-
-// console.log(document.querySelector('#current').parentNode);
-// console.log(document.querySelector('.class').parentNode);
-// console.log(document.querySelector('#current').parentNode.parentNode); //! Родитель родителя от ребенка
-
-//! Получение элемента РОДИТЕЛЯ
-// console.log(document.querySelector('#current').parentElement);
-
+// console.log(document.querySelector('#current').parentNode); // первый родитель
+// console.log(document.querySelector('#current').parentNode.parentNode); // второй родитель
 
 
 //? Получение через дата атрибута
-//! Получение узла - Нода
-// console.log(document.querySelector('[data-current="3"]').nextSibling);
-// console.log(document.querySelector('[data-current="3"]').previousSibling);
-// console.log(document.querySelector('[data-yourName="3"]').previousSibling);
+//! Получает перенос строки
+// console.log(document.querySelector('[data-current="3"]').nextSibling); // следующий перенос строки
+// console.log(document.querySelector('[data-current="3"]').previousSibling); // предыдущий перенос строки
 
-//! Получение элемента СЛЕДУЮЩЕГО ИЛИ ПРЕДИДУЩЕГО ЭЛЕМЕНТА
-// console.log(document.querySelector('[data-current="3"]').nextElementSibling);
-// console.log(document.querySelector('[data-current="3"]').previousElementSibling);
-// console.log(document.querySelector('[data-yourName="3"]').previousElementSibling);
-
-
+//! Получает элемент
+// console.log(document.querySelector('[data-current="3"]').nextElementSibling); // следующий элемент
+// console.log(document.querySelector('[data-current="3"]').previousElementSibling); // предыдущий элемент
 
 //!  Переберающая функция NODE-list (for of)
 for (let node of document.body.childNodes) {
-    if (node.nodeName == '#text') {
-        continue;
-    }
-    console.log(node);
+  if (node.nodeName == '#text') {
+    continue;
+  }
+
+
+  console.log(node);
 }
