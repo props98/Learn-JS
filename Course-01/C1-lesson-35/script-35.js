f78d6a6e6621ff48fe690bca34fe248495bc377e
Jhon <jhon@mail.com>
2022-10-20 10:12:20 +0300

    fix script-35

--- Course-01/C1-lesson-35/script-35.js
+++ Course-01/C1-lesson-35/script-35.js
@@ -5,17 +5,18 @@ console.log(p);
 
 
 //! Динамически вставляем скрипт на страницу
-const script = document.createElement('script');
-script.src = 'js/test.js';
-script.async = false;
-document.body.apppend(script);
+// const script = document.createElement('script');
+// script.src = 'test.js';
+// script.async = false;
+// document.body.append(script);
+
 
 function loadScript(src) {
     const script = document.createElement('script');
     script.src = src;
-    script.async = false;
-    document.body.apppend(script);
+    // script.async = false;
+    document.body.append(script);
 }
 
-loadScript('js/test.js');
-loadScript();
+loadScript('test.js');
+loadScript('test2.js');
