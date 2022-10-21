c7626b00dd32537128fe7357131538597cb8c322
Jhon <jhon@mail.com>
2022-10-21 10:09:45 +0300

    add object plunc

--- Course-01/C1-lesson-20/script.js
+++ Course-01/C1-lesson-20/script.js
@@ -2,3 +2,30 @@
 
 //* Objects, деструктуризация объектов
 
+const options = {
+  name: 'test',
+  width: 1024,
+  height: 1024,
+  colors: {
+    border: 'black',
+    bg: 'tomato'
+  }
+};
+
+// console.log(options.name);
+// console.log(options.colors.bg);
+
+//! Удаление свойства объекта при помощи «delete»
+// delete options.name
+// console.log(options);
+
+for (let key in options) {
+  if (typeof(options[key]) === 'object') {
+    for (let i in options[key]) {
+      console.log(`Свойство: ${i} имеет значение ${options[key][i]} в объекте OPTIONS[COLORS]`);
+    }
+  } else {
+    console.log(`Свойство: ${key} имеет значение ${options[key]} в объекте OPTIONS`);
+  }
+};
+
