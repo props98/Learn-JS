19327399542fcd3b19514b32fb71b03b4145fbdb
Jhon <jhon@mail.com>
2022-10-24 09:56:21 +0300

    passed lesson

--- Course-03/C3-lesson-56/script.js
+++ Course-03/C3-lesson-56/script.js
@@ -1,2 +1,23 @@
 'use strict';
 
+////////* API - Aplication Programming Interface
+// Код программного обеспечения
+
+////////* Fletch API
+// для проверки сайт JSONPlaceholder - https://jsonplaceholder.typicode.com
+
+//* Fetch запрос
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//   .then(response => response.json())
+//   .then(json => console.log(json));
+
+fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify({name: 'Alex'}),
+    headers: {
+      'Content-type': 'application/json'
+    }
+  })
+  .then(response => response.json())
+  .then(json => console.log(json));
+
