9a9065036961664affce3a5483ac21f7cb53676b
Jhon <jhon@mail.com>
2022-10-25 10:01:44 +0300

    add filter method

--- Course-03/C3-lesson-57/script.js
+++ Course-03/C3-lesson-57/script.js
@@ -1,2 +1,10 @@
 'use strict';
 
+//* Методы перебора массива
+
+//* method «Filter»
+const names = ['Alexia', 'John', 'Neo', 'Moana', 'Konstantin'];
+const shortNames = names.filter( (name) => {
+  return name.length < 5;
+});
+console.log(shortNames);
