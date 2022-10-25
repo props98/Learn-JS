5fed87c65458b38f71744cfe6b8623688f48ab19
Jhon <jhon@mail.com>
2022-10-25 10:09:41 +0300

    method Map

--- Course-03/C3-lesson-57/script.js
+++ Course-03/C3-lesson-57/script.js
@@ -9,3 +9,16 @@ const shortNames = names.filter( (name) => {
 });
 
 console.log(shortNames);
+
+//* method «Map»
+const answers = ['IvAn', 'AnnA', 'HELlo'];
+const result = answers.map( item => {
+  return item.toLowerCase();
+});
+
+console.log(result);
+
+//! Не стоит так делать
+let answers2 = ['IvAn', 'AnnA', 'HELlo'];
+answers2 = answers2.map( item => item.toLowerCase());
+console.log(answers2);
