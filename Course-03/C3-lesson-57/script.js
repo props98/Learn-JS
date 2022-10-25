974a592e84b3c7bef07d78289e346fedad2ec648
Jhon <jhon@mail.com>
2022-10-25 10:26:16 +0300

    method Reduce

--- Course-03/C3-lesson-57/script.js
+++ Course-03/C3-lesson-57/script.js
@@ -23,7 +23,19 @@ let answers2 = ['IvAn', 'AnnA', 'HELlo'];
 answers2 = answers2.map( item => item.toLowerCase());
 console.log(answers2);
 
-//* method Every / Some - выдает true & false
+//* method «Every / Some» - выдает true & false
 const some = [4, 2, 4];
 // console.log(some.some(item => typeof(item) === 'number')); // проверяет один элемент
 console.log(some.every(item => typeof(item) === 'number')); // проверяет все элементы по условию
+
+//* method «Reduce»
+const arr = [4, 5, 1, 2, 3, 6];
+const result2 = arr.reduce((sum, current) => {
+  return sum + current
+}, 3); // Принимает аргумени начального числа
+console.log(result2);
+
+const str = ['apple', 'pear', 'plum'];
+const resStr = str.reduce((sum, current) => `${sum}, ${current}`);
+console.log(resStr);
+
