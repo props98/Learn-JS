cb0ab30920f4ec8e5ccfc8082ae493606ebd4a8f
Jhon <jhon@mail.com>
2022-10-25 10:15:37 +0300

    methods every & some

--- Course-03/C3-lesson-57/script.js
+++ Course-03/C3-lesson-57/script.js
@@ -22,3 +22,8 @@ console.log(result);
 let answers2 = ['IvAn', 'AnnA', 'HELlo'];
 answers2 = answers2.map( item => item.toLowerCase());
 console.log(answers2);
+
+//* method Every / Some - выдает true & false
+const some = [4, 2, 4];
+// console.log(some.some(item => typeof(item) === 'number')); // проверяет один элемент
+console.log(some.every(item => typeof(item) === 'number')); // проверяет все элементы по условию
