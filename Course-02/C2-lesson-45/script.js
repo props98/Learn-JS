e7a75867cd7c7de7aa9cf73408528afd42fb7f4f
Jhon <jhon@mail.com>
2022-10-26 11:12:41 +0300

    lesson 45

--- Course-02/C2-lesson-45/script.js
+++ Course-02/C2-lesson-45/script.js
@@ -1,2 +1,28 @@
 'use setrict'
 
+//* Функции конструкторы
+
+function User(name, id) {
+  this.name = name;
+  this.id = id;
+  this.human = true;
+  this.hello = function() {
+    console.log(`Hello ${this.name}`);
+  };
+}
+
+// Добавление метода или свойства через «prototype» если нет доступа к конструктору
+User.prototype.exit = function() {
+  console.log(`User ${this.name} left`);
+}
+
+const neo = new User('Neo', 25);
+const jhon = new User('Jhon', 40);
+
+neo.exit();
+
+neo.hello();
+jhon.hello();
+
+console.log(neo);
+console.log(jhon);
