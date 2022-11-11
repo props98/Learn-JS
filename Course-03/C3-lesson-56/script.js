150f86b410fc1622042487f4a0808c2cf2201d28
Jhon <jhon@mail.com>
2022-11-11 10:21:49 +0300

    ff lesson 56

--- Course-03/C3-lesson-56/script.js
+++ Course-03/C3-lesson-56/script.js
@@ -7,17 +7,3 @@
 // для проверки сайт JSONPlaceholder - https://jsonplaceholder.typicode.com
 
 //* Fetch запрос
-// fetch('https://jsonplaceholder.typicode.com/todos/1')
-//   .then(response => response.json())
-//   .then(json => console.log(json));
-
-fetch('https://jsonplaceholder.typicode.com/posts', {
-    method: 'POST',
-    body: JSON.stringify({name: 'Alex'}),
-    headers: {
-      'Content-type': 'application/json'
-    }
-  })
-  .then(response => response.json())
-  .then(json => console.log(json));
-
