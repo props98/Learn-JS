52eabe9dbcf1953dae35b1f9f519e2370b2e9dd3
Jhon <jhon@mail.com>
2022-11-13 16:27:12 +0300

    ff script lesson 14

--- Course-01/C1-lesson-14/script.js
+++ Course-01/C1-lesson-14/script.js
@@ -2,35 +2,7 @@
 
 //* Циклы
 
-let num = 50;
 
-//* Первый цикл 
-// while (num <= 55) {
-//     console.log(num);
-//     num++;
-// }
-
-//* Второй цикл
-// do {
-//     console.log(num);
-//     num++;
-// }
-// while (num < 55);
-
-//* Третий цикл
-// for (let i = 1; i < 8; i++) {
-//     console.log(num);
-//     num++;
-// }
-
-//* Цикл с отановкой «break» / «continue»
-for (let i = 1; i < 10; i++) {
-    if (i  === 6) {
-        // break;
-        continue;
-    }
-    console.log(i);
-}
 
 //////////////////? Задачи
 
