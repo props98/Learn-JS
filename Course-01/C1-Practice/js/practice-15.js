388138ec7ff4ec01a0572b7146ea3863a7afe859
Jhon <jhon@mail.com>
2022-11-18 18:44:54 +0300

    add practice 15

--- Course-01/C1-Practice/js/practice-15.js
+++ Course-01/C1-Practice/js/practice-15.js
@@ -21,9 +21,32 @@ const personalMovieDB = {
   privat: false
 };
 
-const lastWatchMovie = prompt('Один из последних просмотренных фильмов?', '');
-const rateFilm = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[lastWatchMovie] = rateFilm;
+for (let i = 0; i < 2; i++) {
+  let lastWatchMovie = prompt('Один из последних просмотренных фильмов?', '');
+  let rateFilm = prompt('На сколько оцените его?', '');
+
+  if (lastWatchMovie != null && rateFilm != null && lastWatchMovie != '' && rateFilm != '' && lastWatchMovie.length < 5) {
+    personalMovieDB.movies[lastWatchMovie] = rateFilm;
+    console.log('done');
+  } else {
+    console.error('error');
+    i--;
+  }
+  
+}
+
 
 console.log(personalMovieDB);
+
+
+if (personalMovieDB.count < 10) {
+  console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+  console.log('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+  console.log('Вы киноман');
+} else {
+  console.log('Произошла ошибка');
+};
+
