9777028bf8c5847c11bc81b37f7017e1978aedc1
Jhon <jhon@mail.com>
2023-01-06 15:37:58 +0300

    Training lesson 47

--- Course-02/C2-lesson-47/script.js
+++ Course-02/C2-lesson-47/script.js
@@ -7,35 +7,3 @@
 // 2. Название класса всекда начинаеться с большой буквы
 
 // Концепция
-class Rectangle {
-  constructor(width, height) { // Создание экземпляра класса
-    this.width = width;
-    this.height = height;
-  }
-
-  caclArea() { // Создание методов класса - Имя метода без function и :
-    return this.width * this.height;
-  } // в конце точку с запятой не ставят
-}
-
-class ColoredRectangleWithText extends Rectangle { // Наследуемый класс от «Rectangle»
-  constructor (width, height, text, bgColor) {
-    super(width, height); // Чтобв не копировать свойства и сделать их наследуемыми / всегда первой строчкой
-    this.text = text;
-    this.bgColor = bgColor;
-  }
-
-  showMyProps() {
-    console.log(`Text: ${this.text}, color: ${this.bgColor}`);
-  }
-}
-
-const div = new ColoredRectangleWithText(25, 10, 'Hello', 'Tomato');
-div.showMyProps();
-console.log(div.caclArea());
-
-const square = new Rectangle(10, 20);
-const long = new Rectangle(30, 5);
-
-console.log(square.caclArea()); // Вызываем метод класса square
-console.log(long.caclArea()); // Вызываем метод класса square
