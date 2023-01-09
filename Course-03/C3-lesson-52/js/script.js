342409b369f88cd2e05d36a978fdef42292da2d0
Jhon <jhon@mail.com>
2023-01-09 10:48:16 +0300

    Passed lesson 52

--- Course-03/C3-lesson-52/js/script.js
+++ Course-03/C3-lesson-52/js/script.js
@@ -1,11 +1,45 @@
 'use strict';
 
-//* Технология AJAX - общение с сервером
+//* Технология AJAX (Асинхронное выполнение задач) - общение с сервером
 // Asinchronius Java and XML
+const inputRub = document.querySelector('#rub'),
+      inputUsd = document.querySelector('#usd');
 
+inputRub.addEventListener('input', () => {
+  const request = new XMLHttpRequest();
 
-//! Первое событие «readystatechange»
+  //* Запрос
+  // request.open(method, url, async, login, pass);
+  request.open('GET', 'js/current1.json');
+  request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+  request.send();
 
+  // status
+  // statusText
+  // response
+  // readyState
 
-//! Второе событие «load»
+  //! Первое событие «readystatechange»
+  // request.addEventListener('readystatechange', () => {
+  //   if (request.readyState === 4 && request.status === 200) {
+  //     console.log(request.response);
+  //     const data = JSON.parse(request.response);
+  //     inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
+  //   } else {
+  //     inputUsd.value = 'Something wrong!';
+  //   }
+  // });
+
+  //! Второе событие «load»
+  request.addEventListener('load', () => {
+    if (request.status === 200) {
+      const data = JSON.parse(request.response);
+      inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
+    } else {
+      inputUsd.value = 'Something wrong!';
+    }
+  });
+
+});
+
 
