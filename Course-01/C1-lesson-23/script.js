fbad659a47b164f7c9a54cdf201250e32fe08aed
Jhon <jhon@mail.com>
2023-01-16 10:00:40 +0300

    Repeat lesson 23

--- Course-01/C1-lesson-23/script.js
+++ Course-01/C1-lesson-23/script.js
@@ -2,12 +2,38 @@
  
 //* ООП в JS >>> прототипное ориентированное прогроммирование
 //* Объектно ориентированное програмирование
+let str = 'some';
+let strObj = new String(str);
+console.log(typeof(str));
+console.log(typeof(strObj));
 
+console.dir([1,2,3]);
 
-//! Устарувший формат свойство «__proto__»
+const worker = {
+  health: 100,
+  skills: 50,
+  sayHello: function() {
+    console.log('Hello');
+  }
+};
 
+const john = {
+  health: 80
+};
+
+//! Устаревший формат добавление свойства «__proto__»
+// john.__proto__ = worker;
+// console.log(john.skills);
+// john.sayHello();
 
 //! Новый способ формата свойств 
 //* 1 - Object.setPrototypeOf(john, man) - Установили прототип «man» для «john»
+Object.setPrototypeOf(john, worker); // добавление свойст и методов от  «рабочего» для Джона
+// john.sayHello();
+
 //* 2 - Object.create(solder) - Создали объект крирпый будет прототипно наследоватся от прототипа «man»
+const mike = Object.create(worker);
+// mike.sayHello();
+
 //* 3 - getPrototypeOf
+console.log(Object.getPrototypeOf(john));  // john наследуемый прототип от «worker»
