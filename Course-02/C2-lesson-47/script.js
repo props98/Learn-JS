b8a3d10212ccf64639b96f4d6f98b4d59edcdb42
Jhon <jhon@mail.com>
2023-02-02 18:05:54 +0300

    repeat lesson 47

--- Course-02/C2-lesson-47/script.js
+++ Course-02/C2-lesson-47/script.js
@@ -8,9 +8,38 @@
 
 // Концепция
 class Rectangle {
-  constructor()
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  calcArea() {
+    return this.width * this.height;
+  }
+
+}
+
+// Наследуемость классов от «Rectangle»
+class ColoredRectangleWithText extends Rectangle {
+  constructor(width, height, text, bgColor) {
+    super(width, height); // Вызов свойств от родителя
+
+    this.text = text;
+    this.bgColor = bgColor;
+  }
+
+  showMyProps() {
+    console.log(`Text: ${this.text}, color: ${this.bgColor}`);
+  }
 }
 
 
+// Экземпляры объекта
+// const square = new Rectangle(10, 10);
+// const long = new Rectangle(50, 10);
+// console.log(square.calcArea());
+// console.log(long.calcArea());
 
-// Наследование класса
+const div = new ColoredRectangleWithText(25, 10, 'Hello', 'tomato');
+div.showMyProps();
+console.log(div.calcArea());
