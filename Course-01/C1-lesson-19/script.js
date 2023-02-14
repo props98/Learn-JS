cfe3bd506ccc7a3768313472a5186e2503725e5b
Jhon <jhon@mail.com>
2023-02-14 13:39:59 +0300

    Repeat lesson 19

--- Course-01/C1-lesson-19/script.js
+++ Course-01/C1-lesson-19/script.js
@@ -2,7 +2,27 @@
 
 //* Сallback функция
 
+function first() {
+  // do something
+  setTimeout(function() {
+    console.log(1);
+  }, 500);
+}
 
+function second() {
+  console.log(2);
+}
 
-// Пример
-first('first', () => {
+first();
+second();
+
+function learnJS(lang, callback) {
+  console.log(`I learn: ${lang}`);
+  callback();
+}
+
+function done() {
+  console.log('I pass this lesson!');
+}
+
+learnJS('JavaScript', done);
