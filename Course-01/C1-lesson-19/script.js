00a1ff56adcc1caff53197f5e8f401a4d152c623
Jhon <jhon@mail.com>
2023-02-14 13:30:40 +0300

    start repeat lesson 19

--- Course-01/C1-lesson-19/script.js
+++ Course-01/C1-lesson-19/script.js
@@ -2,37 +2,7 @@
 
 //* Сallback функция
 
-function first(lang, callback) {
-  // Do something
-  setTimeout(function() {
-    console.log(`${lang} function`);
-  }, 500); // Задержка в пол секунды
-  callback();
-}
-
-// function second() {
-//   console.log('second function');
-// }
-
-// first();
-// second();
 
 
 // Пример
 first('first', () => {
-  console.log('second function');
-});
-
-
- 
-function learnJS(lang, callback) {
-  console.log(`I learn ${lang}`);
-  callback();
-}
-
-function done() {
-  console.log('I\'m callback function');
-}
-
-learnJS('JavaScript', done);
-
