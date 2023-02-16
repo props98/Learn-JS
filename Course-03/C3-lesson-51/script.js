bd16e1dacad06472002f569c69004d3678dcfe4b
Jhon <jhon@mail.com>
2023-02-16 13:20:42 +0300

    Repeat lesson 51

--- Course-03/C3-lesson-51/script.js
+++ Course-03/C3-lesson-51/script.js
@@ -4,12 +4,31 @@
 //* JSON - Java Script Object Notation
 //* Глубокое клонирование объектов
 
-
-
+const person = {
+  name: 'Neo',
+  tel: '+75555555'
+};
 
 // Изменяем обычный объект в данные JSON методом «stringify()»
-// Выходит объект с двойными ковычками
-// Изменяем полученный JSON файл в обычный объект методом «parse()»
+// console.log(JSON.stringify(person));
+const stringJson = JSON.stringify(person);
+console.log(stringJson); // Выходит объект с двойными кaвычками
 
+// Изменяем полученный JSON файл в обычный объект методом «parse()»
+console.log(JSON.parse(stringJson));
 
 //* Глубокое клонирование объекта
+const person2 = {
+  name: 'Neo',
+  tel: '+75555555',
+  parents: {
+    mom: 'Ann',
+    dad: 'Mike'
+  }
+};
+
+const clone = JSON.parse(JSON.stringify(person2));
+clone.parents.mom = 'Jane'
+
+console.log(person2);
+console.log(clone);
