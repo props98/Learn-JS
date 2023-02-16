b7df0292922769b07708c3823adf820cd1db0b88
Jhon <jhon@mail.com>
2023-02-16 13:01:58 +0300

    Repeat lesson 56

--- Course-03/C3-lesson-56/script.js
+++ Course-03/C3-lesson-56/script.js
@@ -6,6 +6,18 @@
 ////////* Fletch API
 // для проверки сайт JSONPlaceholder - https://jsonplaceholder.typicode.com
 fetch('https://jsonplaceholder.typicode.com/todos/1')
-  
+  .then(response => response.json())
+  .then(json => console.log(json));
 
 //* Fetch запрос
+fetch('https://jsonplaceholder.typicode.com/posts', {
+  method: 'POST',
+  body: JSON.stringify({
+    name: 'Neo',
+    headers: {
+      'Content-type': 'application/json'
+    }
+  })
+})
+  .then(response => response.json())
+  .then(json => console.log(json));
