df3d53029a3d0221ddf3aa5fc0bf5a8932871cc0
Jhon <jhon@mail.com>
2023-02-20 18:26:31 +0300

    Repeat lesson 6

--- Course-01/C1-lesson-6/script.js
+++ Course-01/C1-lesson-6/script.js
@@ -1,13 +1,37 @@
 "use strict";
 
 //* Класификация типов данных
+// 8 типов данных
 
+let number = 4; // Number
 
+console.log(-4 / 0); // -Infinity
+console.log('string' * 9); // NaN
 
-//! Null
+const person1 = 'Neo'; // String
+const person2 = `Trinity`; // String
 
+const bool = true; // Boolean «true - Yes/ false - No»
 
-//! Udefined (холодильник)
+//* Null
+console.log(something); // ReferenceError - Ошибка ссылочного типа
 
+//* Udefined (холодильник)
+let und;
+console.log(und); // undefined
 
-//! Object
+//* Object
+const obj = {
+  name: 'Neo',
+  age: 25,
+  human: true
+};
+
+console.log(typeof(obj.name));
+console.log(obj.name);
+console.log(typeof(obj['age']));
+console.log(obj['age']);
+
+//* Array - нумерация с нуля
+let arr = ['apple', 'orange', 'banana', 5, {}, []];
+console.log(arr[0]);
