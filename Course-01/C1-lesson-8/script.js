3fcea4467b34f4fe801cd12f72576fadd7f81d63
Jhon <jhon@mail.com>
2023-02-21 11:15:33 +0300

    Start repeat lesson 8

--- Course-01/C1-lesson-8/script.js
+++ Course-01/C1-lesson-8/script.js
@@ -2,13 +2,8 @@
 
 //* Интерполяция
 
-const category = 'toys';
+
 
 //! старый образец записи
-//? console.log('https://someurl.com/' + category + '/' + 4);
 
 //! Интерполяция 
-console.log(`https://someurl.com${category}/5`);
-
-const user = 'Ivan';
-alert(`Hello, ${user}`);
