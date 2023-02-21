e97c61691afbfa13eab1cf9cd462995ab9477576
Jhon <jhon@mail.com>
2023-02-21 11:20:17 +0300

    Repeat lesson 8

--- Course-01/C1-lesson-8/script.js
+++ Course-01/C1-lesson-8/script.js
@@ -2,8 +2,14 @@
 
 //* Интерполяция
 
+const catergory = 'toys';
 
-
-//! старый образец записи
+//! старый образец записи - конкотенация строк
+console.log('https://someurl.com/' + catergory + '/' + '5');
 
 //! Интерполяция 
+console.log(`https://someurl.com/${catergory}/10`);
+
+// Пример.....
+const user = 'Neo';
+console.log(`Hello, ${user}`);
