ea6cc5b1c2229e040dcf6418fa7f692e39dc7e07
Jhon <jhon@mail.com>
2023-02-23 18:31:36 +0300

    Repeat Practice lesson 12

--- Course-01/C1-Practice/js/practice-12.js
+++ Course-01/C1-Practice/js/practice-12.js
@@ -21,3 +21,26 @@
     }
 
 Проверить, чтобы все работало без ошибок в консоли */
+
+// #1
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+// #2
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+}
+
+// #3
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
