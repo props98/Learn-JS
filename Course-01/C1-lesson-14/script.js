416337b3144f90b529eed3cdb3b07b23b3582a66
Jhon <jhon@mail.com>
2023-02-23 17:59:55 +0300

    Repeat lesson 14

--- Course-01/C1-lesson-14/script.js
+++ Course-01/C1-lesson-14/script.js
@@ -2,17 +2,34 @@
 
 //* Циклы
 
+let num = 50;
 
-//* Цикл «while»
-
+//* Цикл «while» - выполняеться пока условие дает true
+// while (num <= 55) {
+//   console.log(num + ' while');
+//   num++; //! обязательный итератор 
+// };
 
 //* Цикл «do while»
+// do {
+//   console.log(num + ' do while');
+//   num++;
+// } 
+// while (num <= 55);
 
+//* Цикл «for of»
+for (let i = 1; i <= 5; i++) {
+  console.log(num + ' for');
+  num++;
+};
 
-// Цикл «for of»
-
-
-// «for of» / «break» & «continue»
-
+// «break» & «continue»
+for (let i = 1; i < 10; i++) {
+  if (i == 5) {
+    break;  // Останавливаеться на 4
+    // continue; // Пропускает цифру 5
+  }
+  console.log(i);
+};
 
  
