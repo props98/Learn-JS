504381ab35d3fd6f3997f65ed4f5fe17078fe73c
Jhon <jhon@mail.com>
2023-02-23 17:47:14 +0300

    Start repeat lesson 14

--- Course-01/C1-lesson-14/script.js
+++ Course-01/C1-lesson-14/script.js
@@ -1,33 +1,18 @@
 "use strict";
 
 //* Циклы
-let num = 50;
+
 
 //* Цикл «while»
-// while (num <= 55) {
-//   console.log(num);
-//   num++;
-// }
+
 
 //* Цикл «do while»
-// do {
-//   console.log(num);
-//   num++;
-// } while (num <= 55);
+
 
 // Цикл «for of»
-for (let i = 1; i <= 5; i++) {
-  console.log(num);
-  num++;
-}
+
 
 // «for of» / «break» & «continue»
-for (let i = 1; i <= 5; i++) {
-  if (i === 3) {
-    // break;
-    continue;
-  }
-  console.log(i);
-}
+
 
  
