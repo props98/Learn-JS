7ad051e27cee50c35e63ca2c45ea8600b2ab0dca
Jhon <jhon@mail.com>
2023-02-24 14:53:13 +0300

    Start repeat lesson 16

--- Course-01/C1-lesson-16/script.js
+++ Course-01/C1-lesson-16/script.js
@@ -2,45 +2,17 @@
 
 //* Функции, стрелочные функции
 
-let numGlobal = 30; // Глобальная переменная
 
 //* Function declaration -
 //* Создается до начала выполнения скрипта, можно вызывать перед объявлением
-function showMyFirstMessage(a, b) {
-  console.log(a + b);
-  numGlobal = 35;
-  let numLocal = 20; // локальная перменная - сущ. внутри функции
-  console.log(numGlobal);
-}
 
-function calc(a, b) {
-  return (a + b);
-}
-
-function ret() {
-  let num = 50;
-  return num;
-}
-
-const anotherNum = ret();
-console.log(anotherNum);
-
-console.log(calc(2, 2));
-showMyFirstMessage(4, 5);
 
 //* Function expression - 
 //! Вызываеться ТОЛЬКО после объявления
 //* Создается только тогда, когда доходит поток кода, можно вызвать только после объявления
-let logger = function() {
-  console.log('hello');
-};
 
-logger();
 
 
 //* Стрелочные функции -
 //* Не имеет своего контескта (this) - контекста вызова
-/*const calc = (a, b) => a + b;*/ //*Если помещается в одну строчку
-
-const calc2 = (a, b) => a + b;
-console.log(calc2(1, 2));
+//*Если помещается в одну строчку
