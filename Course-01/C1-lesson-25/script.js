fc29b6e8402f6731c90b6bc045b26471cd0f6d8a
Jhon <jhon@mail.com>
2023-03-01 18:02:31 +0300

    Start repeat lesson 25

--- Course-01/C1-lesson-25/script.js
+++ Course-01/C1-lesson-25/script.js
@@ -1,22 +1,2 @@
 'use strict';
 
-function hello() {
-  console.log('Hello World!');
-  debugger;
-}
-hello();
-
-function hi() {
-  console.log('Say hi!');
-}
-hi();
-
-const arr = [1, 2, 7, 4, 3],
-      sorted = arr.sort(compareNum);
-
-function compareNum(a, b) {
-  return a - b;
-}
-
-
-console.log(sorted);
