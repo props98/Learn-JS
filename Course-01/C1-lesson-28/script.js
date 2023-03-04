433b3787ca4d269adb732900ec7b12da4b95214c
Jhon <jhon@mail.com>
2023-03-04 17:30:01 +0300

    Start repeat lesson 28

--- Course-01/C1-lesson-28/script.js
+++ Course-01/C1-lesson-28/script.js
@@ -2,29 +2,9 @@
 
 //* Получение элементов со страницы
 
-const box = document.getElementById('box');
-console.log(box);
-
-const btns = document.getElementsByTagName('button');
-console.log(btns);
-btns[0].style.background = 'purple';
-
-const circles = document.getElementsByClassName('circle');
-console.log(circles);
-
 
 
 //* Современные методы получения элементов со страницы
 
-const hearts = document.querySelectorAll('.heart');
-console.log(hearts);
-hearts.forEach(heart => {
-    console.log(heart);
-});
 
 // Получение первого элемента со страницы
-const oneHeart = document.querySelector('.heart');
-console.log(oneHeart);
-
-const btn = document.querySelector('button');
-console.log(btn);
