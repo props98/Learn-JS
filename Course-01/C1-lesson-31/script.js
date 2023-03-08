fb364d868548e8941d82c6433a35b98cd9d23359
Jhon <jhon@mail.com>
2023-03-08 19:02:44 +0300

    Repeat lesson 31

--- Course-01/C1-lesson-31/script.js
+++ Course-01/C1-lesson-31/script.js
@@ -2,4 +2,52 @@
 
 //* События и их обработчики
 
+const btn = document.querySelector('button'),
+      overlay = document.querySelector('.overlay');
 
+//! Лучше Не использовать - если свойство повторяеться, 
+//! выполняеться последний обработчик,
+//! нельзя удалить обработчик.
+// btn.onclick = function() {
+//   console.log('JS script onclick');
+// }
+
+// btn.addEventListener('click', () => {
+//   console.log('addEventListener click 1');
+// });
+
+// btn.addEventListener('click', () => {
+//   console.log('addEventListener click 2');
+// });
+
+// btn.addEventListener('mouseenter', (evt) => {
+//   console.log(evt.target);
+//   evt.target.remove();
+// });
+
+// let i = 0;
+// const deleteElem = (evt) => {
+//   console.log(evt.target);
+//   console.log(evt.type);
+//   // i++;
+//   // if (i == 1) {
+//   //   btn.removeEventListener('click', deleteElem);
+//   // }
+// };
+
+// btn.addEventListener('click', deleteElem);
+// overlay.addEventListener('click', deleteElem);
+
+
+// const link = document.querySelector('a');
+// link.addEventListener('click', (evt) => {
+//   evt.preventDefault();
+//   console.log(evt.target);
+// });
+
+const btns = document.querySelectorAll('button');
+btns.forEach(btn => {
+  btn.addEventListener('click', (evt) => {
+    console.log(evt.target);
+  })
+})
