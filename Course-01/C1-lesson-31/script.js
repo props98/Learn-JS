26fd238a8ff96731d033e33078ea8c457e1c2f9b
Jhon <jhon@mail.com>
2023-03-08 18:31:33 +0300

    Start repeat lesson 31

--- Course-01/C1-lesson-31/script.js
+++ Course-01/C1-lesson-31/script.js
@@ -2,44 +2,4 @@
 
 //* События и их обработчики
 
-const btn = document.querySelector('button');
-const overlay = document.querySelector('.overlay');
 
-// btn.onclick = function() { //! Старый способ
-//   alert('click');          //! Уже не исаользуют
-// };
-
-// btn.onclick = function() { //! Старый способ
-//   alert('Second click');   //! Второй клик не сработает
-// };
-
-let i = 0;
-
-const deleteElement = (evt) => {
-      console.log(evt.target);
-      // console.log(evt.currentTarget); // Всплытие событий от вложенного элемента к родителю
-      // console.log(evt.type);
-      // evt.target.remove();
-      // i++;
-      // if (i == 2) {
-      //   btn.removeEventListener('click', deleteElement);
-      // }
-    }
-
-
-// btn.addEventListener('click', deleteElement);
-// overlay.addEventListener('click', deleteElement);
-
-const link = document.querySelector('a');
-
-// link.addEventListener('click', (evt) => {
-//   evt.preventDefault(); //Отмена радного события
-
-//   console.log(evt.target);
-// });
-
-const btns = document.querySelectorAll('button');
-
-btns.forEach(btn => {
-  btn.addEventListener('click', deleteElement, {once: true})
-});
