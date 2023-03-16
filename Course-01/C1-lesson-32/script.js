9c747dabc02ca39b66c40fc18fe3a0069d962d29
Jhon <jhon@mail.com>
2023-03-16 14:10:21 +0300

    Repeat lesson 32

--- Course-01/C1-lesson-32/script.js
+++ Course-01/C1-lesson-32/script.js
@@ -4,16 +4,38 @@
 //* [data-атрибуты]
 //* Преимущества «for of»
 
-//? Отталкиваемся от родителя
+//? Отталкиваемся от родителя - получение НОДЫ
+// console.log(document.body);
+// console.log(document.head);
+// console.log(document.documentElement);
+// console.log(document.body.childNodes); // Получение всех узлов
+// console.log(document.body.firstChild);
+// console.log(document.body.lastChild);
 
-
-//! Получение элемента
+//! Получение элемента - Получение узла
 //? Получение родителя от ребенка
+// console.log(document.querySelector('#current').parentNode.parentNode);
+// console.log(document.querySelector('[data-current="3"]').nextSibling);
+// console.log(document.querySelector('[data-current="3"]').previousSibling);
 
+//! Получение элемента - без НОДЫ
+// console.log(document.body.lastElementChild);
+// console.log(document.body.firstElementChild);
+// console.log(document.querySelector('#current').parentElement);
+// console.log(document.querySelector('#current').nextElementSibling);
 
 //? Получение через дата атрибута
-//! Получает перенос строки
+// console.log(document.querySelector('[data-current="3"]').nextElementSibling);
+// console.log(document.querySelector('[data-current="3"]').previousElementSibling);
 
-//! Получает элемент
 
 //!  Переберающая функция NODE-list (for of)
+// console.log(document.body.childNodes);
+
+for (let node of document.body.childNodes) {
+  if (node.nodeName == '#text') {
+    continue; 
+  }
+
+  console.log(node);
+}
