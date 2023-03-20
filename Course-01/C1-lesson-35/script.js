0986f3da82f062cac4f03ea755a14356f793e0e5
Jhon <jhon@mail.com>
2023-03-20 16:51:44 +0300

    Repeat lesson 35

--- Course-01/C1-lesson-35/script.js
+++ Course-01/C1-lesson-35/script.js
@@ -2,7 +2,23 @@
 
 //* Async, defer - Динамические скрипты
 
-
+const p = document.querySelectorAll('p');
+console.log(p);
 
 
 //! Динамически вставляем скрипт на страницу
+//! Ведут себя как async
+// const script = document.createElement('script');
+// script.src = 'test.js';
+// script.async = false; // Отменяем поведение async
+// document.body.append(script);
+
+function loadScript(src) {
+  const script = document.createElement('script');
+  script.src = src;
+  script.async = false; // Отменяем поведение async
+  document.body.append(script);
+}
+
+loadScript('test.js');
+loadScript('test2.js');
