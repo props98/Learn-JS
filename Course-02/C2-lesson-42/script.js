05f0791c29a4c65fe3e19eac66f6f2d8ce79f8b0
Jhon <jhon@mail.com>
2023-03-24 16:07:55 +0300

    Repeat lesson 42

--- Course-02/C2-lesson-42/script.js
+++ Course-02/C2-lesson-42/script.js
@@ -2,24 +2,46 @@
 
 //* Параметры документа окна и работа с ними
 
+// console.dir(document);
 
+const box = document.querySelector('.box'),
+      btn = document.querySelector('button');
 
-//* Получение размеров всего окная -15px прокукрутки и рамки снизу
+//* Ширина и высота с паддингами минус 15px «прокрутка»
+// const clientWidth = box.clientWidth;
+// const clientHeight = box.clientHeight;
+// console.log(clientWidth, clientHeight);
 
-//* Получение размеров окна как указаных в стилях вся видимая часть 
+//* Ширина и высота с маргинами
+const offsetWidth = box.offsetWidth;
+const offsetHeight = box.offsetHeight;
+console.log(offsetWidth, offsetHeight);
 
-//* Получение размеров контента что не помещаеться в видимую часть -15px прокрутка
+//* Полная высота элемента и та что скрыта
+const scrollHeight = box.scrollHeight;
+console.log(scrollHeight);
+
 
 // При клике открываеться весь скрытый тексл за скролом
+btn.addEventListener('click', (evt) => {
+  // box.style.height = box.scrollHeight + 'px';
+  console.log(box.scrollTop);
+})
 
 // Получение всех координат окна методом «getBoundingClientRect»
+console.log(box.getBoundingClientRect());
+console.log(box.getBoundingClientRect().top);
 
 // Получение информацию какие стили приенены к опр. элементу «Cumputed»
-
-// Узнаем размер видимой части окна браузера
-
+const style = window.getComputedStyle(box);
+console.log(style);
+console.log(style.display);
 
 
 // //* Премещение относительно того места на котором остановились (x, y)
+console.log(document.documentElement.clientWidth);
+console.log(document.documentElement.scrollTop);
 
 // //* Премещение с начала страницы (x, y)
+window.scrollBy(0, 400);
+window.scrollTo(0, 400);
