507973a4dd30627530d3064aae2839c3b5a23c07
Jhon <jhon@mail.com>
2023-03-25 13:53:26 +0300

    Repeat lesson 45

--- Course-02/C2-lesson-45/script.js
+++ Course-02/C2-lesson-45/script.js
@@ -3,11 +3,35 @@
 //* Функции конструкторы
 
 // Старый вид записи
+// const num = new Number(3);
+// const newFunc = new Function(3)
+// console.log(newFunc);
 
+function User(name, id) {
+  this.name = name;
+  this.id = id;
+  this.human = true;
+
+  this.hello = function() {
+    console.log(`Hello, ${this.name}!`);
+  }
+}
 
 // Метод «Prototype»
-// Будет прототипно наследоваться, и будет у всех потомков после него, 
 // Если нет доступа к конструктору
+// Будет прототипно наследоваться, и будет у всех потомков после него, 
+User.prototype.exit = function() {
+  console.log(`User ${this.name} left`);
+}
 
+const neo = new User('Neo', 25);
+const trinity = new User('Trinity', 20);
 
+// console.log(neo);
+// console.log(trinity);
+neo.hello();
+trinity.hello();
 
+// add method with prototype
+neo.exit();
+trinity.exit();
