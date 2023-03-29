e63125e2dba0bdfcd73ea5aa54bcd0383597477d
Jhon <jhon@mail.com>
2023-03-29 11:38:19 +0300

    Repeat lesson 57

--- Course-03/C3-lesson-57/script.js
+++ Course-03/C3-lesson-57/script.js
@@ -2,31 +2,77 @@
 
 //* Методы перебора массива
 
+//! Метод «forEach» - Перебирает переданный массив, не создает новый
 //* Метод «Filter» - Возвращает новый отфильтрованный массив
+const names = ['Neo', 'Trin', 'Morphius', 'Konstantine'];
+const shortNames = names.filter((name) => {
+  console.log(name);
+  return name.length < 5;
+});
+console.log(shortNames);
 
 
-//* Метод «Map» - Трансформирующий метод
+//* Метод «Map» - Трансформирующий метод, на выходе новый массив
+const answers = ['nEo', 'tRInitY', 'HeLLo'];
+const resultAnswers = answers.map((answer) => {
+  return answer.toLowerCase();
+});
+console.log(resultAnswers);
+
+// Сокращенная запись с методом map();
+const resultShortAnswers = answers.map(answer => answer.toLocaleLowerCase());
+console.log(resultShortAnswers);
+
+//* Можно не создавать новую переменную, а перезависать исходную с возвращенным новым массивом
+//! Лучше создавать отдельную переменную для возращаемого нового массива
+let cars = ['bMW', 'MeRCedec', 'JEEP'];
+cars = cars.map(car => car.toLocaleLowerCase());
+console.log(cars);
 
 
 //* Методы «Every / Some» - возвращает булиновое значение true & false
-const some = [4, 3, 3];
+//! some() проверяет что хотябы один элемент должен соответствовать условию
+const some = [4, 'neo', 'sdf'];
+console.log(some.some(item => typeof(item) === 'number'));
 
-// console.log(some.some(item => typeof(item) === 'number')); // SOME - есть хотя бы один элемент подходит под условя
-// EVERY -  если все элементы подходят под условия
+//! every() -  если все элементы подходят под условия
+console.log(some.every(item => typeof(item) === 'number')); // false
+
+const every = [3, 4, 5];
+console.log(every.every(item => typeof(item) === 'number')); // true
+
 
 //* method «Reduce» - схлопование массива в единое целое
+//! Возвращает новый результат
+const arr = [4, 5, 1, 3, 2, 6];
+          // 0   4             // 3   4 третий аргумент 3
+          // 4   5 -> начало   // 7   5
+          // 9   1             // 12  1
+          // 10  3 ...etc      // 13  3 ...etc
 
-            // 0   3
-            // 3   4 -> начало
-            // 7   5
-            // 12  6 ...etc
-
-// const res = arr.reduce((sum, current) => sum + current); // 29
+const res = arr.reduce((sum, current) => sum + current, 3); 
+console.log(res);
 
 
 //* Example /////////////
+const arrFruit = ['apple', 'pear', 'plum'];
+const resFruit = arrFruit.reduce((fruit, current) => `${fruit}, ${current}`);
+console.log(resFruit);
 
 
-// 1. Создание матрицы из массивов
-// 2. Перебор вложенного массива по второму индексу «person»
-// 3. Перебор массива и возвращение первого индекса
+// 1. Из объекта делаем митрицу массивов с помощью Object.entries
+// 2. По цепочке делаем перебор полученой матрицы массива:
+  // 2.1 Перебор вложенного массива по второму индексу «person» //! filter();
+  // 2.1 Перебор массива и возвращение первого индекса //! map();
+const obj = {
+  neo: 'person',
+  trin: 'person',
+  dog: 'animal',
+  cat: 'animal'
+}
+
+let newArr = Object.entries(obj)
+  .filter(item => item[1] === 'person')
+  .map(item => item[0]);
+
+console.log(newArr);
