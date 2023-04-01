e0559f738ec396cdf6ce85303f023a7151309a1c
Jhon <jhon@mail.com>
2023-04-01 13:51:26 +0300

    Repeat lesson 68

--- Course-03/C3-lesson-68/script.js
+++ Course-03/C3-lesson-68/script.js
@@ -2,4 +2,21 @@
 
 //* СВОЙСТВА - Акцессоры / Геттры и Cеттеры - Свойства объекта
 
+const person = {
+  name: 'Neo',
+  age: 25,
+
+  get userAge() {
+    return this.age;
+  },
+
+  set userAge(num) {
+    this.age = num
+  }
+};
+
+console.log(person.userAge); // get
+console.log(person.userAge = 35); // set
+
+console.log(person.userAge);
 
