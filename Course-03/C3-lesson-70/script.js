1cd412347b305103c5099cfff675557ebf56a2e0
Jhon <jhon@mail.com>
2023-04-02 12:23:09 +0300

    Repeat lesson 70

--- Course-03/C3-lesson-70/script.js
+++ Course-03/C3-lesson-70/script.js
@@ -2,3 +2,35 @@
 
 //* «Модульное» разбиение кода
 
+// const app = '123';
+
+const number = 1;
+
+//! Ананнимная самовызывающая функция -> Function Expression
+(function() {
+  let number = 2;
+
+  console.log(number);
+  console.log(number + 3);
+}());
+
+console.log(number);
+
+//! Экспорт методов и свойств через «Объектный интерфейс»
+const user = (function() {
+  const privat = function() {
+    console.log('I am privat function!');
+  };
+
+  const helloName = function() {
+    console.log('Hello, Neo!');
+  };
+
+  return {
+    sayHello: privat,
+    helloName: helloName
+  };
+}());
+
+user.sayHello();
+user.helloName();
