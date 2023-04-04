2deb025ba7be3da65fb8f37035e10c1520db0830
Jhon <jhon@mail.com>
2023-04-04 18:02:55 +0300

    Start repeat lesson 72

--- Course-03/C3-lesson-72/src/js/main.js
+++ Course-03/C3-lesson-72/src/js/main.js
@@ -1,12 +1,6 @@
 'use strict';
 
 //* Именованный экспорт
-export let one = 1;
 
-let two = 2;
-export {two};
 
 //* Экспорт по умолчанию «default»
-export default function sayHi() {
-  console.log('Hello');
-}
--- Course-03/C3-lesson-72/src/js/script.js
+++ Course-03/C3-lesson-72/src/js/script.js
@@ -1,11 +1,6 @@
 'use strict';
 
 //* Ипорт всех сущностей
-import * as data from './main.js';
+
 
 //* Импорт по умолчанию
-import sayHi from './main.js';
-
-// console.log(first);
-console.log(`${data.one} and ${data.two}`);
-sayHi();
