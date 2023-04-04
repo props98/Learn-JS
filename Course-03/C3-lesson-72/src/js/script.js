0039a2b1d545be57699f9d4a13ccc1d2183a8de0
Jhon <jhon@mail.com>
2023-04-04 18:20:06 +0300

    Repeat lesson 72

--- Course-03/C3-lesson-72/src/js/script.js
+++ Course-03/C3-lesson-72/src/js/script.js
@@ -1,6 +1,25 @@
 'use strict';
 
 //* Ипорт всех сущностей
+// Для именнованого синтаксиса, передача переменных из другого модуля.
+// Импорт в ввиде деструктуризации
+import {one, two} from './main.js';
+console.log(`${one} and ${two}`);
+
+// Можно сразу переименовать переданную переменную
+import {one as first} from './main.js';
+console.log(first);
+
+// Иморт всех переменных и функций с другого модуля
+// И сразу можно переименовать
+import * as data from './main.js';
+
+// Чтобв обратиться
+// console.log(data.one);
+// console.log(data.two);
+data.sayHy();
 
 
 //* Импорт по умолчанию
+import sayBye from './main.js';
+sayBye();
