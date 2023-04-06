6abf47fa7e87de086b088ac75119c18873965dec
Jhon <jhon@mail.com>
2023-04-06 14:45:54 +0300

    Start repeat lesson 75

--- Course-03/C3-lesson-75/script.js
+++ Course-03/C3-lesson-75/script.js
@@ -3,31 +3,3 @@
 // Проверка над ошибками в браузере
 //* Try / Catch
 
-// try {
-//   console.log('Good');
-//   console.log(a);
-//   console.log('result');
-// } catch(error) {
-//   console.log(error.name);
-//   console.log(error.message);
-//   console.log(error.stack);
-// } finally {
-
-// }
-
-// console.log(a);
-// console.log('Stil normal');
-
-
-// Example
-
-
-try {
-  document.querySelector('.active').addEventListener('click', () => {
-    console.log('click');
-  });
-} catch(error) {
-  // console.log(error);
-}
-
-console.log('Normal');
