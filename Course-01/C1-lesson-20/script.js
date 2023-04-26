fc0d2a5731f035d72a1b23337e3881dec0a743db
Jhon <jhon@mail.com>
2023-04-26 15:27:59 +0300

    Repeat lesson 20

--- Course-01/C1-lesson-20/script.js
+++ Course-01/C1-lesson-20/script.js
@@ -3,24 +3,57 @@
 //* Objects, деструктуризация объектов
 
 //! Вариант возможен, но не используеться
-// const obj = new Object(); 
+//! const obj = new Object();
+
+const options = {
+  name: 'test',
+  width: 1024,
+  height: 1024,
+  colors: {
+    border: 'black',
+    background: 'red'
+  },
+  makeTest: function() {
+    console.log('Test');
+  }
+};
 
 //* Созданный метод объекта
-
-//! Созданный метод объекта
-
-// Перебор объекта поверхностных свойств
+options.makeTest();
 
 //* Деструктуризация объекта
+const {name, width, height} = options;
+console.log(width);
+const {border, background} = options.colors;
+console.log(border);
 
 
-//* Узнаем сколько свойств у объекта / без вложенных
-//! Старый и не удобный метод подсчета свойств в объекте
+// Перебор объекта поверхностных свойств и вложенных свойств
+
+let counter = 0;
+
+for (let key in options) {
+  if (typeof(options[key]) === 'object') {
+    for (let i in options[key]) {
+      console.log(`Свойства: ${i} имеет значение ${options[key][i]}`);
+      counter++;
+  }} else {
+    console.log(`Свойства: ${key} имеет значение ${options[key]}`);
+    counter++;
+  }
+}
+
+console.log(counter);
+
 
 //! Удаление свойства объекта при помощи «delete»
+// delete options.name;
+// console.log(options);
 
 //! Перебор вложенных объектов «for (let item in object)»
 //* Встроенный метод 
 //* Делаем из свойств объекта массив «Object.keys()»
+console.log(Object.keys(options));
 
 //* Делаем подсчет массива свойством «length»
+console.log(Object.keys(options).length);
